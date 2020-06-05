ea428691834ae5fadbda0723b063c835d3059b46
c011727829 <jixeis9541@gmail.com>
2020-06-05 02:05:43 +0000

    ソートに変更

--- struct/task2-3.c
+++ struct/task2-3.c
@@ -8,31 +8,26 @@ struct Data {
 };
 
 
-void search(struct Data list[], char *key) {
-  for(int i = 0; i < 4; i++) {
-    if(strcmp(list[i].name, key) == 0) {
-      printf("Index: %d\n", list[i].id);
-      return;
-    }
-  }
-  printf("Not found\n");
-  return;
-}
-
-
 int main() {
   struct Data list[4] = {{0, "Kameda"},
 			 {1, "Ono"},
 			 {2, "Iwashita"},
 			 {3, "Aida"}};
-  char key[20];
+  struct Data tmp;
 
-  printf("Name: ");
-  scanf("%s", key);
+  for(int i = 0; i < 4; i++) {
+    for(int j = i + 1; j < 4; j++) {
+      if(strcmp(list[i].name, list[j].name) > 0) {
+	tmp = list[i];
+	list[i] = list[j];
+	list[j] = tmp;
+      }
+    }
+  }
 
-  search(list, key);
-  
+  for(int i = 0; i < 4; i++) {
+    printf("%d: %s\n", list[i].id, list[i].name);
+  }
+    
   return 0;
 }
-
-
